c4e771a8a8b3197dada46166671dcf9611d82f7f
Sin.js <sinmineryt@gmail.com>
2021-07-09 14:41:12 +0200

    Remove main

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,12 @@
 #![no_main]
 use core::panic::PanicInfo;
 
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     loop {}
 }
 
-fn main() {}
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
