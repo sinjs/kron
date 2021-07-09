03b419504a8410f7f8dbf26601882126c734ceef
Sin.js <sinmineryt@gmail.com>
2021-07-09 15:18:22 +0200

    Add Hello world source code! :^)

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,19 @@
 #![no_main]
 use core::panic::PanicInfo;
 
+static MESSAGE: &[u8] = b"Hello from Kron!";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in MESSAGE.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0x7;
+        }
+    }
+
     loop {}
 }
 
