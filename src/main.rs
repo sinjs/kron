24c900094f24c81d661be15b0f3adc36601366ac
Sin.js <50749166+sinmineryt@users.noreply.github.com>
2021-07-16 17:31:14 +0200

    Tests: Add assertion (eq 1, 1)

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 #![no_main]
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 mod vga_buffer;
@@ -16,9 +17,19 @@ fn test_runner(tests: &[&dyn Fn()]) {
     }
 }
 
+#[test_case]
+fn test_assertion() {
+    print!("Test assertion... ");
+    assert_eq!(1, 1);
+    println!("OK");
+}
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
 
     loop {}
 }
