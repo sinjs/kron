c7a9fafe59845a941fc61e7ae8897439e421883b
Sin.js <50749166+sinmineryt@users.noreply.github.com>
2021-07-16 17:55:35 +0200

    Kernel/Serial: Add serial printing function and macro

--- src/serial.rs
+++ src/serial.rs
@@ -9,3 +9,26 @@ lazy_static! {
         Mutex::new(serial_port)
     };
 }
+
+#[doc(hidden)]
+pub fn _print(args: ::core::fmt::Arguments) {
+    use core::fmt::Write;
+    SERIAL1
+        .lock()
+        .write_fmt(args)
+        .expect("Printing to serial failed");
+}
+
+#[macro_export]
+macro_rules! serial_print {
+    ($($arg:tt)*) => {
+        $crate::serial::_print(format_args!($($arg)*));
+    };
+}
+
+#[macro_export]
+macro_rules! serial_println {
+    () => ($crate::serial_print!(concat!($fmt, "\n")));
+    ($fmt:expr) => ($crate::serial_print!(concat!($fmt, "\n")));
+    ($fmt:expr, $($arg:tt)*) => ($crate::serial_print!(concat!($fmt, "\n"), $($arg)*));
+}
