b1e12dc527acc5d54a8afaec15f3fb1fb3685b55
Sin.js <50749166+sinmineryt@users.noreply.github.com>
2021-07-16 18:03:38 +0200

    Tests: Show test status automatically

--- src/main.rs
+++ src/main.rs
@@ -10,20 +10,33 @@ mod vga_buffer;
 
 static MESSAGE: &[u8] = b"Hello from Kron!";
 
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("OK");
+    }
+}
+
 #[cfg(test)]
 fn test_runner(tests: &[&dyn Fn()]) {
     serial_println!("Running {} tests", tests.len());
     for test in tests {
-        test();
+        test.run();
     }
     exit_qemu(QemuExitCode::Success);
 }
 
 #[test_case]
 fn test_assertion() {
-    serial_print!("Test assertion... ");
     assert_eq!(1, 1);
-    serial_println!("OK");
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
