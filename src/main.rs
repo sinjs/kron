f4d7167b3746e064035c84fe9c030b9e12a12786
Sin.js <50749166+sinmineryt@users.noreply.github.com>
2021-07-16 17:18:41 +0200

    Tests: Add test framework

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,16 @@ mod vga_buffer;
 
 static MESSAGE: &[u8] = b"Hello from Kron!";
 
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
